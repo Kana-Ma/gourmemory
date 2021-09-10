bcb606436667ac28d6e3e43e058992ec4df8860a
Kana-Ma <kana.3611@icloud.com>
2021-09-10 18:15:13 +0900

    お店一覧機能（トップページ）の実装

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -2,6 +2,7 @@ class ShopsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
+    @shops = Shop.order('created_at DESC')
   end
 
   def new
