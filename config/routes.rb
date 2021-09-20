55e4d73e310f6d7d58c16f2e9ed4c47d8de5c81d
Kana-Ma <kana.3611@icloud.com>
2021-09-20 12:24:52 +0900

    ユーザー個人ページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "shops#index"
+
   resources :points, only: [:new, :create] do
     collection do
       get 'search'
     end
   end
+
   resources :shops, only: [:new, :create, :show, :edit, :update, :destroy] do
     collection do
       get 'search'
       get 'shop_search'
     end
   end
+
+  resources :users, only: :show
 end
