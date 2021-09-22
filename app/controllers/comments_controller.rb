1ed9bc37a3e5ac0431a42769b127add9dbcfb881
Kana-Ma <kana.3611@icloud.com>
2021-09-22 17:31:09 +0900

    Rubocop実行後の修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,7 @@ class CommentsController < ApplicationController
   end
 
   private
+
   def comment_params
     params.require(:comment).permit(:comment).merge(user_id: current_user.id, shop_id: params[:shop_id])
   end
